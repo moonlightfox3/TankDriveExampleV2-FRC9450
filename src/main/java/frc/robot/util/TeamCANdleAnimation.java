be2eb4e4a899527dc0988008fd30ee4be512a1bc
Joe M. <tiredredstoner@gmail.com>
2026-02-03 21:12:24 -0800

    Custom animation progress - Colors

--- src/main/java/frc/robot/util/TeamCANdleAnimation.java
+++ src/main/java/frc/robot/util/TeamCANdleAnimation.java
@@ -13,33 +13,15 @@ import frc.robot.subsystems.LEDSubsystem;
 public class TeamCANdleAnimation extends Command {
   private LEDSubsystem m_leds;
 
-  private static final double BLUE_H = 239.0;
-  private static final double BLUE_S = 1.0;
-  private static final double BLUE_V = 0.11;
-  private static final double BLUE_B = 1.0;
-  private static final double ORANGE_H = 4.0;
-  private static final double ORANGE_S = 1.0;
-  private static final double ORANGE_V = 1.0;
-  private static final double ORANGE_B = 1.0;
-  private static double BLUEGRADIENT_H;
-  private static double BLUEGRADIENT_S;
-  private static double BLUEGRADIENT_V;
-  private static double BLUEGRADIENT_B;
-  private static double ORANGEGRADIENT_H;
-  private static double ORANGEGRADIENT_S;
-  private static double ORANGEGRADIENT_V;
-  private static double ORANGEGRADIENT_B;
+  public static final RGBWColor BLUE = RGBWColor.fromHSV(239.0, 1.0, 0.11);
+  public static final RGBWColor ORANGE = RGBWColor.fromHSV(4.0, 1.0, 1.0);
+  public static final RGBWColor BLUE_GRADIENT = new RGBWColor((BLUE.Red + ORANGE.Red) / 3, (BLUE.Green + ORANGE.Green) / 3, (BLUE.Blue + ORANGE.Blue) / 3);
+  public static final RGBWColor ORANGE_GRADIENT = new RGBWColor((BLUE.Red + ORANGE.Red) / 3 * 2, (BLUE.Green + ORANGE.Green) / 3 * 2, (BLUE.Blue + ORANGE.Blue) / 3 * 2);
 
   /** Creates a new TeamCANdleAnimation. */
   public TeamCANdleAnimation(LEDSubsystem leds) {
     addRequirements(leds);
     m_leds = leds;
-
-    RGBWColor blue = RGBWColor.fromHSV(BLUE_H, BLUE_S, BLUE_V).scaleBrightness(BLUE_B);
-    RGBWColor orange = RGBWColor.fromHSV(ORANGE_H, ORANGE_S, ORANGE_V).scaleBrightness(ORANGE_B);
-    // TODO: Gradient colors (below) - separate (2 between colors)
-    RGBWColor blueGradient = new RGBWColor((blue.Red + orange.Red) / 2, (blue.Green + orange.Green) / 2, (blue.Blue + orange.Blue) / 2);
-    RGBWColor orangeGradient = new RGBWColor((blue.Red + orange.Red) / 2, (blue.Green + orange.Green) / 2, (blue.Blue + orange.Blue) / 2);
   }
 
   // Called when the command is initially scheduled.
