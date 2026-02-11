5f4666a8d3464d8ab20535e892a71fa698872895
Joe M. <tiredredstoner@gmail.com>
2026-02-10 20:13:55 -0800

    LED animation

--- src/main/java/frc/robot/commands/TeamCANdleAnimationCommand.java
+++ src/main/java/frc/robot/commands/TeamCANdleAnimationCommand.java
@@ -21,17 +21,15 @@ public class TeamCANdleAnimationCommand extends Command {
   public static final double FRAMERATE = 6.0;
   private final double BASE_BRIGHTNESS = 1.0;
 
-  // Color data
+  // Config - Color data
   public static final double[] BLUE_HSV = {240.0, 1.0, 0.16}; // RGB 0, 0, 40
   public static final double[] ORANGE_HSV = {4.0, 1.0, 1.0}; // RGB 255, 17, 0
 
-  // Computed colors
+  // Color data structures
   public static final RGBWColor BLUE = RGBWColor.fromHSV(BLUE_HSV[0], BLUE_HSV[1], BLUE_HSV[2]);
   public static final RGBWColor ORANGE = RGBWColor.fromHSV(ORANGE_HSV[0], ORANGE_HSV[1], ORANGE_HSV[2]);
   // public static final RGBWColor BLUE_GRADIENT = new RGBWColor(0, 26, 165);
   // public static final RGBWColor ORANGE_GRADIENT = new RGBWColor(255, 41, 0);
-
-  // Color arrays
   private ArrayList<RGBWColor> COLORS = new ArrayList<>();
   private ArrayList<RGBWColor> COLORS_CANDLE = new ArrayList<>();
 
@@ -46,13 +44,13 @@ public class TeamCANdleAnimationCommand extends Command {
     addRequirements(leds);
     m_leds = leds;
 
+    // Config - Color arrays
+
     COLORS.add(BLUE);
-    addBlendedColorsToArray(COLORS, BLUE, new RGBWColor(10, 20, 20), 3);
-    addBlendedColorsToArray(COLORS, new RGBWColor(35, 15, 2), ORANGE, 3);
+    addBlendedColorsToArray(COLORS, BLUE, /*BLUE.scaleBrightness(0.01)*/new RGBWColor(10, 20, 20), 3);
+    addBlendedColorsToArray(COLORS, /*ORANGE.scaleBrightness(0.01)*/new RGBWColor(35, 15, 2), ORANGE, 3);
     COLORS.add(ORANGE);
-    
-    int sizeCOLORS = COLORS.size();
-    for (int i = sizeCOLORS - 2; i >= 1; i--) COLORS.add(COLORS.get(i));
+    addReversedColorsToArray(COLORS);
 
     COLORS_CANDLE.add(BLUE);
     COLORS_CANDLE.add(BLUE);
@@ -63,19 +61,11 @@ public class TeamCANdleAnimationCommand extends Command {
     COLORS_CANDLE.add(ORANGE);
     COLORS_CANDLE.add(ORANGE);
 
-    for (int i = 0; i < COLORS.size() / 2; i++) {
-      RGBWColor temp = COLORS.get(i);
-      COLORS.set(i, COLORS.get(COLORS.size() - 1 - i));
-      COLORS.set(COLORS.size() - 1 - i, temp);
-    }
-    for (int i = 0; i < COLORS_CANDLE.size() / 2; i++) {
-      RGBWColor temp = COLORS_CANDLE.get(i);
-      COLORS_CANDLE.set(i, COLORS_CANDLE.get(COLORS_CANDLE.size() - 1 - i));
-      COLORS_CANDLE.set(COLORS_CANDLE.size() - 1 - i, temp);
-    }
-
-    for (int i = 0; i < COLORS.size(); i++) COLORS.set(i, COLORS.get(i).scaleBrightness(BASE_BRIGHTNESS));
-    for (int i = 0; i < COLORS_CANDLE.size(); i++) COLORS_CANDLE.set(i, COLORS_CANDLE.get(i).scaleBrightness(BASE_BRIGHTNESS));
+    // Color arrays final setup
+    applyBaseBrightnessToArray(COLORS);
+    applyBaseBrightnessToArray(COLORS_CANDLE);
+    reverseArray(COLORS);
+    reverseArray(COLORS_CANDLE);
   }
   private void addBlendedColorsToArray(ArrayList<RGBWColor> arr, RGBWColor color1, RGBWColor color2, int totalCount) {
     double posAdd = 1.0 / (totalCount + 1);
@@ -86,6 +76,21 @@ public class TeamCANdleAnimationCommand extends Command {
         (int) ((1 - t) * color1.Green + t * color2.Green),
         (int) ((1 - t) * color1.Blue + t * color2.Blue)
       ));
+    }
+  }
+  private void addReversedColorsToArray(ArrayList<RGBWColor> arr) {
+    int size = arr.size();
+    for (int i = size - 2; i >= 1; i--) arr.add(arr.get(i));
+  }
+  private void applyBaseBrightnessToArray(ArrayList<RGBWColor> arr) {
+    for (int i = 0; i < arr.size(); i++) arr.set(i, arr.get(i).scaleBrightness(BASE_BRIGHTNESS));
+  }
+  private void reverseArray(ArrayList<RGBWColor> arr) {
+    for (int i = 0; i < arr.size() / 2; i++) {
+      int endI = arr.size() - 1 - i;
+      RGBWColor temp = arr.get(i);
+      arr.set(i, arr.get(endI));
+      arr.set(endI, temp);
     }
   }
 
